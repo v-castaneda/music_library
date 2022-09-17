f577c734f05685bf2c5d2bf2d149ed2388da3e46
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-17 09:03:21 -0400

    updated searchBar to dynamically update form as typing begins

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,8 +7,10 @@ const SearchBar = (props) => {
         <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
             <input 
                 type='text'
-                placeholder='Enter a search term here' 
-                onChange={(e) => setSearchTerm(e.target.value)}
+                placeholder='Search Here' 
+                onChange={
+                    (e) => props.handleSearch(e, e.target.value)
+                }
             />
             <input 
                 type='submit'
