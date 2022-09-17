6c43a6ead097fa41a1b07a4eada565c1fb442474
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-17 07:33:01 -0400

    removed boilerplate code, and added music app wireframe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import SearchBar from './components/SearchBar';
+import Gallery from './components/Gallery';
 
-function App() {
+const App = () => {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
+      {message}
+      <Gallery />
     </div>
   );
 }
