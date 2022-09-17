c744b22e325ba486c3ae3b04c3b27b5ff895050d
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-17 08:51:27 -0400

    rendering data for each item data array

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,15 @@
 import GalleryItem from "./GalleryItem"
 
 const Gallery = (props) => {
+    const display = props.data.map((item, index) => {
+        return (
+            <GalleryItem item={item} key={index}/>
+        )
+    })
+
     return (
         <div>
-            <GalleryItem />
+            {display}
         </div>
     )
 }
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,11 +3,30 @@ import { useState } from 'react'
 const GalleryItem = (props) => {
     let [view, setView] = useState(false)
 
+    const simpleView = () => {
+        return (
+            <div>
+                <h3>{props.item.trackName}</h3>
+                <h4>{props.item.collectionName}</h4>
+            </div>
+        )
+    }
+
+    const detailView = () => {
+        return (
+            <div>
+                <h2>{props.item.trackName}</h2>
+                <h3>{props.item.collectionName}</h3>
+                <h4>{props.item.primaryGenreName}</h4>
+                <h4>{props.item.releaseDate}</h4>
+            </div>
+        )
+    }
+
     return (
-        <div 
-            onClick={() => setView(!view)}
+        <div onClick={() => setView(!view)}
             style={{'display': 'inline-block'}}>
-            <p>One Gallery Item</p>
+            {view ? detailView() : simpleView()}
         </div>
     )
 }
