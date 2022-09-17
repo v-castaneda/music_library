5fc64040d37abe2dbed0e1ec882f11b9292674ad
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-17 08:57:58 -0400

    adding style to GalleryItem

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -5,7 +5,7 @@ const GalleryItem = (props) => {
 
     const simpleView = () => {
         return (
-            <div>
+            <div style={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -14,13 +14,31 @@ const GalleryItem = (props) => {
 
     const detailView = () => {
         return (
-            <div>
+            <div style={detailsStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
         )
+    }
+
+    const simpleStyle = {
+        'width': '25vw', 
+        'height': '20vh', 
+        'border': '1px solid black', 
+        'margin': '2px'
+    }
+
+    const detailsStyle = {
+        'width': '80vw', 
+        'height': '20vh', 
+        'border': '1px solid black', 
+        'margin': '2px', 
+        'backgroundImage': `url(${props.item.artworkUrl100})`, 
+        'backgroundRepeat': 'no-repeat', 
+        'backgroundSize': 'cover', 
+        'color': 'yellow'
     }
 
     return (
